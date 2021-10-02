99a50d5f59b24d1c15d5d00aa28b20f15ca351a8
Michael Patterson <mjpatter88@gmail.com>
2021-10-01 22:56:32 -0500

    Refactor tests.

--- cpu_test.go
+++ cpu_test.go
@@ -2,57 +2,57 @@ package main
 
 import "testing"
 
-func TestCpu(t *testing.T) {
-	AssertBreak := func(cpu *Cpu, status bool) {
-		if cpu.Status.Break != status {
-			t.Errorf("Expected Break status to be %t but was %t", status, cpu.Status.Break)
-		}
-	}
-	AssertZero := func(cpu *Cpu, status bool) {
-		if cpu.Status.Zero != status {
-			t.Errorf("Expected Zero status to be %t but was %t", status, cpu.Status.Zero)
-		}
-	}
-	AssertNegative := func(cpu *Cpu, status bool) {
-		if cpu.Status.Negative != status {
-			t.Errorf("Expected Negative status to be %t but was %t", status, cpu.Status.Negative)
-		}
-	}
-	AssertRegisterA := func(cpu *Cpu, value uint8) {
-		if cpu.RegA != value {
-			t.Errorf("Expected registerA to be %#x but was %#x", value, cpu.RegA)
-		}
-	}
-	t.Run("Test BRK", func(t *testing.T) {
-		cpu := Cpu{}
-		cpu.execute([]uint8{BRK})
+func TestBRK(t *testing.T) {
+	cpu := Cpu{}
+	cpu.execute([]uint8{BRK})
 
-		AssertBreak(&cpu, true)
-	})
-
-	t.Run("Test LDA", func(t *testing.T) {
+	AssertBreak(t, &cpu, true)
+}
+func TestLDA(t *testing.T) {
+	t.Run("LDA", func(t *testing.T) {
 		cpu := Cpu{}
 		cpu.execute([]uint8{LDA, 0x4a, BRK})
 
-		AssertRegisterA(&cpu, 0x4a)
-		AssertZero(&cpu, false)
-		AssertNegative(&cpu, false)
+		AssertRegisterA(t, &cpu, 0x4a)
+		AssertZero(t, &cpu, false)
+		AssertNegative(t, &cpu, false)
 	})
 
-	t.Run("Test LDA - Zero flag", func(t *testing.T) {
+	t.Run("Zero flag", func(t *testing.T) {
 		cpu := Cpu{}
 		cpu.execute([]uint8{LDA, 0x00, BRK})
 
-		AssertRegisterA(&cpu, 0x00)
-		AssertZero(&cpu, true)
-		AssertNegative(&cpu, false)
+		AssertRegisterA(t, &cpu, 0x00)
+		AssertZero(t, &cpu, true)
+		AssertNegative(t, &cpu, false)
 	})
 
-	t.Run("Test LDA - Negative flag", func(t *testing.T) {
+	t.Run("Negative flag", func(t *testing.T) {
 		cpu := Cpu{}
 		cpu.execute([]uint8{LDA, 0xF0, BRK})
-		AssertRegisterA(&cpu, 0xF0)
-		AssertZero(&cpu, false)
-		AssertNegative(&cpu, true)
+		AssertRegisterA(t, &cpu, 0xF0)
+		AssertZero(t, &cpu, false)
+		AssertNegative(t, &cpu, true)
 	})
+}
+
+func AssertBreak(t *testing.T, cpu *Cpu, status bool) {
+	if cpu.Status.Break != status {
+		t.Errorf("Expected Break status to be %t but was %t", status, cpu.Status.Break)
+	}
+}
+func AssertZero(t *testing.T, cpu *Cpu, status bool) {
+	if cpu.Status.Zero != status {
+		t.Errorf("Expected Zero status to be %t but was %t", status, cpu.Status.Zero)
+	}
+}
+func AssertNegative(t *testing.T, cpu *Cpu, status bool) {
+	if cpu.Status.Negative != status {
+		t.Errorf("Expected Negative status to be %t but was %t", status, cpu.Status.Negative)
+	}
+}
+func AssertRegisterA(t *testing.T, cpu *Cpu, value uint8) {
+	if cpu.RegA != value {
+		t.Errorf("Expected registerA to be %#x but was %#x", value, cpu.RegA)
+	}
 }
